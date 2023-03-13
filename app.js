9ad4cb8a3fc0b304ee9170ff7fae6b3d423ec10e
ADITYA T B <adityatb2001@gmail.com>
2023-03-13 19:19:23 +0530

    level 3

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 //jshint esversion:6
-require('dotenv').config();
+//require('dotenv').config();
 const express=require("express");
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
 const ejs=require("ejs");
-const encrypt=require("mongoose-encryption");
-
+//const encrypt=require("mongoose-encryption");
+const md5=require("md5");
 
 const app=express();
 
@@ -23,7 +23,7 @@ const userSchema=new mongoose.Schema({
 
 
 
-userSchema.plugin(encrypt,{secret:process.env.secret,encryptedFields:["password"]});
+//userSchema.plugin(encrypt,{secret:process.env.secret,encryptedFields:["password"]});
 
 const User=mongoose.model("user",userSchema);
 
@@ -35,19 +35,18 @@ app.get("/",function(req,res){
 
 app.get("/register",function(req,res){
     res.render("register");
+    
 });
 
 
 app.get("/login",function(req,res){
-    res.render("login");
+   res.render("register");
 });
 
-
 app.post("/register",function(req,res){
-
     const newUser=new User({
         username:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
 
     newUser.save().then(function(){
@@ -57,22 +56,19 @@ app.post("/register",function(req,res){
     .catch(function(err){
         console.log(err);
     });
-});
+})
 
 
 app.post("/login",function(req,res){
     User.findOne({username:req.body.username}).then(function(found){
-        if(found.password===req.body.password){
+        if(found.password===md5req.body.password){
             res.render("secrets");
         }
     })
     .catch(function(err){
         console.log(err);
     });
-});
-
-
-
+})
 
 
 app.listen(3000,function(){
@@ -85,6 +81,33 @@ app.listen(3000,function(){
 
 
 
+// app.post("/register",function(req,res){
+
+//     const newUser=new User({
+//         username:req.body.username,
+//         password:req.body.password
+//     });
+
+//     newUser.save().then(function(){
+//         console.log("saved");
+//         res.render("secrets");
+//     })
+//     .catch(function(err){
+//         console.log(err);
+//     });
+// });
+
+
+// app.post("/login",function(req,res){
+//     User.findOne({username:req.body.username}).then(function(found){
+//         if(found.password===req.body.password){
+//             res.render("secrets");
+//         }
+//     })
+//     .catch(function(err){
+//         console.log(err);
+//     });
+// });
 
 
 
